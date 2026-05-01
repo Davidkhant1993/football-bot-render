00cbc33ff35a3a38c181c55fea6278fa7c3b89ac
Davidkhant1993 <bonekhantaung1993@gmail.com>
2026-05-01 17:51:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -345,6 +345,7 @@ def send_posts():
 # RUN
 # =========================================================
 if SEND_ON_START:
+    send_message("✅ Bot test post is working")
     send_posts()
 
 schedule.every().day.at(DAILY_POST_TIME).do(send_posts)
