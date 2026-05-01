115e561ef0078923c0f1703993953fd1acd8d2ec
Davidkhant1993 <bonekhantaung1993@gmail.com>
2026-05-01 18:41:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -276,7 +276,7 @@ def get_upcoming_matches(league_id):
         {
             "league": league_id,
             "season": SEASON,
-            "next": MATCH_LIMIT_PER_LEAGUE,
+            "date": datetime.now().strftime("%Y-%m-%d"),
         },
     )
 
